c5d6c99a8b0eaf8fb9b832a8c9f1cbf6a8a3df72
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2025-04-17 11:53:51 -0700

    See Work Log

--- Source/FicsitPlanner/Private/FicsitPlanner.cpp
+++ Source/FicsitPlanner/Private/FicsitPlanner.cpp
@@ -1,29 +1,44 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "FicsitPlanner.h"
-
-#include "GameInstanceModuleManager.h"
+#include "AssetRegistryModule.h"
 
 #define LOCTEXT_NAMESPACE "FFicsitPlannerModule"
 
 void FFicsitPlannerModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-
-	// DEBUG: Log "Hello World" to the output log
 	UE_LOG(LogTemp, Warning, TEXT("Hello World from FicsitPlanner!"));
 
-	// Get the main root world component
-	UGameInstanceModuleManager * module = UGameInstanceModuleManager::FindModule("FicsitPlanner");
-	
+	// Get the Asset Registry Module
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<
+		FAssetRegistryModule>("AssetRegistry");
+
+	// Ensure the asset registry is fully loaded
+	AssetRegistryModule.Get().SearchAllAssets(true);
+
+	// Retrieve all assets
+	TArray<FAssetData> AssetDataArray;
+	AssetRegistryModule.Get().GetAllAssets(AssetDataArray, true);
+
+	// Check if any assets were found
+	if (AssetDataArray.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No assets found."));
+		return;
+	}
+
+	// Iterate through the assets and log their names and types
+	for (const FAssetData& AssetData : AssetDataArray)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Found Asset: %s, Type: %i"), *AssetData.GetObjectPathString(),
+		       AssetData.IsAssetLoaded());
+	}
 }
 
 void FFicsitPlannerModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// This function may be called during shutdown to clean up your module. For modules that support dynamic reloading,
 	// we call this function before unloading the module.
 }
 
 #undef LOCTEXT_NAMESPACE
-	
+
 IMPLEMENT_MODULE(FFicsitPlannerModule, FicsitPlanner)
